141bb6ccd3c99e8412e13a13606c4ed8d9894efd
Rasmus <66802136+rasmus234@users.noreply.github.com>
2022-06-10 12:39:48 +0200

    update

--- main.py
+++ main.py
@@ -10,18 +10,43 @@ sys.path.append("./")
 from signalrcore.hub_connection_builder import *
 
 
+
+data = {
+   "Logging":{
+      "LoggerId":"62a1ec85e74a66e76deaab34",
+      "PairingId":"",
+      "Active":False,
+      "SocketUrl":"40.87.132.220:9093",
+      "RestUrl":"40.87.132.220:9092"
+   },
+   "Air":{
+      "MinHumid":1,
+      "MaxHumid":1,
+      "MinTemp":1,
+      "MaxTemp":11
+   },
+   "Soil":{
+      "Moist":1.2,
+      "Dry":3.3
+   }
+}
+
+
 def start_listener():
     hub_connection.on("ReceiveMessage", print)
     argss = None
     # response = hub_connection.send("ConnectLogger", ["rasmus","id"], lambda args: print(args.result))
 
-    util.send(hub_connection,"ConnectLogger",["TestId"],lambda: print("successs"),lambda: print("FAIL"))
+    util.send(hub_connection,"ConnectLogger", ["62a1ec85e74a66e76deaab34"])
+    # util.send(hub_connection,"ConnectLogger", ["62a1ec85e74a66e76deaWRONMG"])
+    hub_connection.on("GetConfig", lambda m: util.send(hub_connection,"SendConfig", [data]))
     # response = hub_connection.send("ConnectLogger", ["TestId"])
 
 
 
 
-server_url = "ws://localhost:5140/hubs/logger"
+# server_url = "ws://localhost:5140/hubs/logger"
+server_url = "https://plant-control-backend.herokuapp.com/hubs/logger"
 
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
--- util.py
+++ util.py
@@ -6,7 +6,6 @@ from signalrcore.hub.base_hub_connection import BaseHubConnection
 def on_result(message):
     print(message.result)
 
-def send(connection: BaseHubConnection, method: str, args: [], on_success, on_error, timeout: int = 2):
 
+def send(connection: BaseHubConnection, method: str, args: []):
     result = connection.send(method, args, lambda m: on_result(m))
-
